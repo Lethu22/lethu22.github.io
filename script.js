b6e94e8f1e367f322e25b9170d3e1a64e274a258
Lethu22 <146815194+Lethu22@users.noreply.github.com>
2024-10-12 15:29:20 +0200

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
-    alert('Thank you for your message!');
-    // Here you can add code to send the form data to your server or email
-});
